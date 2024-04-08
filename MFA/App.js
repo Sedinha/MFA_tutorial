1fecdff0f81dcbf6e8e93c926c76d85e74ff1f4b
Sedinha <121468093+Sedinha@users.noreply.github.com>
2024-04-08 19:24:22 -0300

    ImageCatcher

--- MFA/App.js
+++ MFA/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Button from './components/Button';
 
+import { useState } from 'react';
+
 import ImageViewer from './components/ImageVIewer';
 
 import * as ImagePicker from 'expo-image-picker';
@@ -9,12 +11,16 @@ import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 1,
     });
     if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
       console.log(result);
     } else {
       alert('U dit not select any image.');
@@ -23,10 +29,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
